d4fa404ad8f0c5e17cbc6f346c933165f30d7a70
Satyam Thakur <sat.thakur20@gmail.com>
2024-01-04 21:52:03 +0530

    Creating Auth UI Client class wtih suspend functions

---
+++ app/src/main/java/com/satyamthakur/learning/googlefirebasesignon/presentation/signin/SignInResult.kt
@@ -0,0 +1,12 @@
+package com.satyamthakur.learning.googlefirebasesignon.presentation.signin
+
+data class SignInResult(
+    val data: UserData?,
+    val errorMessage: String?
+)
+
+data class UserData(
+    val userId: String,
+    val userName: String?,
+    val profilePictureUrl: String?
+)
